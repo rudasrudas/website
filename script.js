e5917837327745fdb1ab63b0aaf4ce8795f7af06
rudasrudas <zdanaviciusjustas1@stud.kea.dk>
2021-01-27 16:39:18 +0100

    Update script.js
    added console logging

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ function renderImageOnTriangles(imgData){
         for(let j = 0; j < xTriangles; j++){
             triangleArray[(offsetYt + i) * xTriangles + (offsetXt + j)].thickness = 
                 translatedValues[i * xTriangles + j] * 5;
+            console.log(translatedValues[i * xTriangles + j] * 5);
         }
     }
 }
